48d95f26a9c36411af06d25bc198f5cbb3d20a68
NickBurrows <nick.github1@gmail.com>
2023-04-30 10:50:23 +0800

    更改主題

---
+++ docs/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
